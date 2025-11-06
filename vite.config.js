8b7f2104c4e43f3ecf45cf2dd4acb56745c0199c
JANE HANNA <153586810+Jane121223@users.noreply.github.com>
2025-11-06 17:31:40 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/WT-Healthcare/',  
   plugins: [react()],
+  base: './',
+  build: {
+    outDir: 'dist',
+  },
 })
